456a63144e2bb94de1f0f643419dccbd86c9d94a
Will McCutchen <will@mccutch.org>
2018-06-17 13:11:05 -0700

    Add TTL example

--- example_test.go
+++ example_test.go
@@ -53,3 +53,40 @@ func Example() {
 	// got value for key "foo": 42
 	// got value for key "foo": 42
 }
+
+// ExampleTTL shows how to set a TTL for cache entries
+func ExampleTTL() {
+	var (
+		key       = "foo"
+		val       = 42
+		fillDelay = 250 * time.Millisecond
+	)
+
+	// A simple cache filling function that just waits a constant amount of
+	// time before returning a value
+	filler := func(ctx context.Context, key string) (interface{}, error) {
+		<-time.After(fillDelay)
+		fmt.Printf("computed value for key %q: %d\n", key, val)
+		return val, nil
+	}
+
+	// Create our cache with a 50ms TTL
+	cache := fillcache.New(filler, fillcache.TTL(50*time.Millisecond))
+
+	// Fetch the key every 20ms for 100ms to ensure that the value is recomputed
+	// after the 50ms TTL
+	for i := 0; i < 5; i++ {
+		result, _ := cache.Get(context.Background(), key)
+		fmt.Printf("got value for key %q: %d\n", key, result.(int))
+		<-time.After(20 * time.Millisecond)
+	}
+
+	// Output:
+	// computed value for key "foo": 42
+	// got value for key "foo": 42
+	// got value for key "foo": 42
+	// got value for key "foo": 42
+	// computed value for key "foo": 42
+	// got value for key "foo": 42
+	// got value for key "foo": 42
+}
